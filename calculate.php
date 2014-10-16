791072ebe737aaca0a56d150db79844cba5d8418
keadtisak <chaiyawanlive@gmail.com>
2014-10-17 00:59:57 +0700

    update
    เพิ่มการคำนวนที่ calculate และลด code html ที่ไม่จำเป็น ที่tomyam

--- calculate.php
+++ calculate.php
@@ -9,38 +9,46 @@ $checkr= $_REQUEST['r'];
 $va1 = $_REQUEST['count1'];
 $va2 = $_REQUEST['count2'];
 $va3 = $_REQUEST['count3'];
+$a ="เพิ่ม";
 
 	if(isset($checkp1)&&isset($checkp2)&&isset($checkp3)){
 		if (isset($va1)||isset($va2)||isset($va3)){
 			$cal = 35+(($va1+$va2+$va3)*15);
+			$disp = "$a$checkp1 $va1 $checkp2 $va2 $checkp3 $va3";
 		}
 	}elseif (isset($checkp1)&&isset($checkp2)){
 		if(isset($va1)||isset($va2)){
 			$cal = 35+(($va1+$va2)*15);
+			$disp = "$a$checkp1 $va1 $checkp2 $va2";
 		}
 	}elseif (isset($checkp1)&&isset($checkp3)){
 		if(isset($va1)||isset($va3)){
 			$cal = 35+(($va1+$va3)*15);
+			$disp = "$a$checkp1$va1$checkp3$va3";
 		}
 	}elseif (isset($checkp3)&&isset($checkp2)){
 		if(isset($va3)||isset($va2)){
 			$cal = 35+(($va3+$va2)*15);
+			$disp = "$a$checkp2$va2$checkp3$va3";
 		}
 	}elseif (isset($checkp1)){
 		if(isset($va1)){
 			$cal = 35+($va1*15);
+			$disp = "$a$checkp1$va1";
 		}
 	}elseif (isset($checkp2)){
 		if(isset($va2)){
 			$cal = 35+($va2*15);
+			$disp = "$a$checkp2$va2";
 		}
 	}elseif (isset($checkp3)){
 		if(isset($va3)){
 			$cal = 35+($va3*15);
+			$disp = "$a$checkp3$va3";
 		}
+	}else {
+		$cal = 35;
 	}
-
-echo $checks.$checkn.$checkr."เพิ่ม ".$checkp."จำนวน ".$va1."ราคา".$cal;
 
 ?>
 
@@ -51,11 +59,25 @@ echo $checks.$checkn.$checkr."เพิ่ม ".$checkp."จำนวน ".$va1.
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <link href="bootstrap/css/bootstrap.min.css" rel="stylesheet">
-    <title>ข้อมูลคณะ</title>
+    <title>คำนวนผล</title>
     
 </head>
 
 <body>
+<div class="row">
+  <div class="col-xs-6 col-md-2"></div>
+  <div class="col-xs-6 col-md-8">
+  	<div class="row">
+  		<div class="col-xs-12 col-md-12"><br></div>
+  		<div class="col-xs-12 col-md-12"><h2><?php echo $checks.$checkn.$checkr.$disp." ราคา ".$cal." บาท";?></h2></div>
+  		<div class="col-xs-12 col-md-12"></div>
+	</div>
+  
+  
+  </div>
+  <div class="col-xs-6 col-md-2"></div>
+</div>
+
 
 
 
--- tomyam.php
+++ tomyam.php
@@ -1,11 +1,7 @@
 <?php
  include 'connect.php';
-
- 
  
  ?> 
-
-
 
 
 <!DOCTYPE html>
@@ -14,9 +10,8 @@
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>Bootstrap ร้านต้มยำ</title>
+    <title> ร้านต้มยำ</title>
     <link href="bootstrap/css/bootstrap.min.css" rel="stylesheet">
-
   </head>
 <body>
 
@@ -24,37 +19,37 @@
 <div class="row">
   <div class="col-md-2"></div>
   <div class="col-md-8" style = "background-color: #E0F2F7">
-  
+  <form class="form_input" method="post" action="calculat.php">
   <div class="row">
-  <div class="col-xs-4 col-sm-12"  >qwe </div>
+  <div class="col-xs-4 col-sm-12"  > </div>
   <div class="col-xs-4 col-sm-12"  ><h3>รายการที่สั่ง</h3></div>
   <div class="col-xs-4 col-sm-12"  ><h3 >เส้น </h3></div>
-   	<form class="form_input" method="post" action="too.php">
+   	
    	<div class="col-xs-2 col-sm-3"  > 
          <div class="radio">
   		<label>
-   		 	<input type="radio" name="s" id="optionsRadios1" value="เล้นเล็ก" checked>เล้นเล็ก
+   		 	<input type="radio" name="s"  value="เล้นเล็ก" checked>เล้นเล็ก
   			</label>
 	</div>   
       </div>
       <div class="col-xs-4 col-sm-3">
        <div class="radio">
   		<label>
-   		 	<input type="radio" name="s" id="optionsRadios1" value="เส้นหมี่" checked>เส้นหมี่
+   		 	<input type="radio" name="s" value="เส้นหมี่" checked>เส้นหมี่
   			</label>
 		</div>
       </div>
       <div class="col-xs-4 col-sm-3">
         <div class="radio">
   		<label>
-   		 	<input type="radio" name="s" id="optionsRadios1" value="เส้นบะหมี่" checked>เส้นบะหมี่
+   		 	<input type="radio" name="s" value="เส้นบะหมี่" checked>เส้นบะหมี่
   			</label>
 	</div>
       </div>
    	 	<div class="col-xs-4 col-sm-3">
         <div class="radio">
   		<label>
-   		 	<input type="radio" name="s" id="optionsRadios1" value="วุ้นเส้น" checked>วุ้นเส้น
+   		 	<input type="radio" name="s"  value="วุ้นเส้น" checked>วุ้นเส้น
   			</label>
 	</div>
       </div>
@@ -63,50 +58,64 @@
       <div class="col-xs-2 col-sm-3"  > 
         <div class="radio">
   		<label>
-   		 	<input type="checkbox" name="p" id="checkbox1" value="ไข่มะตูม" checked>ไข่มะตูม
+   		 	<input type="checkbox" name="p1" id="p" value="ไข่มะตูม" >ไข่มะตูม
   			</label>
 	</div>
+	<select name ="count1" id ="dialog-confirm" class="form-control" style=width:100px>
+		<option value="0">-</option>
+  		<option value="1">1</option>
+  		<option value="2">2</option>
+  		<option value="3">3</option>
+  		<option value="4">4</option>
+  		<option value="5">5</option>
+		</select>	
       </div>
+      
       <div class="col-xs-4 col-sm-3">
         <div class="radio">
   		<label>
-   		 	<input type="checkbox" name="p" id="optionsRadios1" value="กุ้ง" checked>กุ้ง
-  			</label>
-  		
-	</div>
-      </div>
-      <div class="col-xs-4 col-sm-3">
-        <div class="radio">
-  		<label>
-   		 	<input type="checkbox" name="p" id="optionsRadios1" value="ปลาหมึก" checked>ปลาหมึก
+   		 	<input type="checkbox" name="p2" id="p" value="กุ้ง" >กุ้ง
   			</label>
 	</div>
-      </div>
-     <div class="col-xs-4 col-sm-3" >
-      <select class="form-control" style=width:100px>
-  		<option>1</option>
-  		<option>2</option>
-  		<option>3</option>
-  		<option>4</option>
-  		<option>5</option>
+	<select name ="count2" id ="dialog-confirm" class="form-control" style=width:100px>
+		<option value="0">-</option>
+  		<option value="1">1</option>
+  		<option value="2">2</option>
+  		<option value="3">3</option>
+  		<option value="4">4</option>
+  		<option value="5">5</option>
 		</select>
-  		</div>
-  		
-   	 	
-   	 	
+      </div>
+      
+      <div class="col-xs-4 col-sm-3">
+        <div class="radio">
+  		<label>
+   		 	<input type="checkbox" name="p3" id="p" value="ปลาหมึก" >ปลาหมึก
+  			</label>
+  			
+	</div><select name ="count3" id ="dialog-confirm" class="form-control" style=width:100px>
+		<option value="0">-</option>
+  		<option value="1">1</option>
+  		<option value="2">2</option>
+  		<option value="3">3</option>
+  		<option value="4">4</option>
+  		<option value="5">5</option>
+		</select>
+      </div>
+
       <!-- line 3 -->
       <div class="col-xs-4 col-sm-12"  ><h3 >น้ำซุป</h3></div>
       <div class="col-xs-2 col-sm-3"  > 
         <div class="radio">
   		<label>
-   		 	<input type="radio" name="n" id="optionsRadios1" value="น้ำใส" checked>น้ำใส
+   		 	<input type="radio" name="n"  value="น้ำใส" checked>น้ำใส
   			</label>
 	</div>
       </div>
       <div class="col-xs-4 col-sm-3">
         <div class="radio">
   		<label>
-   		 	<input type="radio" name="n" id="optionsRadios1" value="ต้มยำ" checked>ต้มยำ
+   		 	<input type="radio" name="n"  value="ต้มยำ" checked>ต้มยำ
   			</label>
 	</div>
       </div>
@@ -117,35 +126,28 @@
       <div class="col-xs-2 col-sm-3"  > 
         <div class="radio">
   		<label>
-   		 	<input type="radio" name="r" id="optionsRadios1" value="เผ็ด" checked>เผ็ด
+   		 	<input type="radio" name="r"  value="เผ็ด" checked>เผ็ด
   			</label>
 	</div>
       </div>
       <div class="col-xs-4 col-sm-3">
         <div class="radio">
   		<label>
-   		 	<input type="radio" name="r" id="optionsRadios1" value="ไม่เผ็ด" checked>ไม่เผ็ด
+   		 	<input type="radio" name="r"  value="ไม่เผ็ด" checked>ไม่เผ็ด
   			</label>
   		</div>
+  		
   		</div>
-  		<input type="submit" class="btn btn-default" value="ยืนยัน">
-      </form>
+  		
       </div>
-      
+      <input type="submit" class="btn btn-default" value="ยืนยัน">
+      <input type="reset" class="btn btn-default" value="ล้าง">
+      </form>
       </div>
 	</div>
       
-   	 	
-   	 		
-   	 		
-   	 		
+ 		
    	 		 <div class="col-md-2"></div>
 
-  
-<?php echo $checkb1;
-
-
-
-?>
 </body>
 </html>
